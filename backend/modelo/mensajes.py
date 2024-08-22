df887a5dd12e98d804dda33c2eeafe56275ab1a4
Dacoop <juancho1601@hotmail.com>
2024-08-21 20:57:58 -0500

    --no-edit

---
+++ backend/modelo/mensajes.py
@@ -0,0 +1,10 @@
+class Mensajes:
+    #MENSAJES DE ERROR
+    NOT_FOUND = "No se encontro la pelicula"
+    NOT_FOUND_ID = "No se encontro la pelicula con id:"
+    EXCEPTION_MESSAGE = "No se puede procesar"
+    
+    #
+    CREATE_MESSAGE = "Se ha CREADO el recurso correctamente"
+    MODIFY_MESSAGE = "Se ha MODIFICADO el recurso correctamente"
+    DELETE_MESSAGE = "Se ha ELIMINADO el recurso correctamente"
